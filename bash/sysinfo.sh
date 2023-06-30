0036dea7d2609e20f17b86812b03eb196263890c
Manjit Kaur <maanmanjit38@gmail.com>
2023-06-29 21:06:18 -0400

    updated sysinfo lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 #This command is used to get sysinfo.sh
-echo "FQDN:"
-hostname -f
 
-#This command is used to get host information
-echo "Host Information!"
-hostnamectl
+fqdn=$(hostname -f)
 
-This command is used to get ip address
-echo "IP Address!"
-hostname -I
+#This command to display OS info to get required data
+#FNR == 7 is used for selecting the line 7and print $3, $4, $5
+hostname=$(hostnamectl | awk 'FNR == 7 {print $3, $4, $5}')
 
-#this command is used to get root filesystem status
-echo "Root Filesystem Status!"
-df / -h
+#This command is used to get ip address
+ip=$(hostname -I)
+
+#df command with '/' for root directory and '-h' option for human readable format with awk command
+#FNR == 2 is used for selecting the line 2 and print $4 prints the 4th item field
+freespace=$(df / -h | awk 'FNR == 2 {print $4}')
+
+
+cat <<EQF
+
+Report
+=========================================
+FQDN: $fqdn
+Operating system name and version : $hostname
+IP Address: $ip
+Root Filesystem Free Space: $freespace
+=========================================
+
+EQF
